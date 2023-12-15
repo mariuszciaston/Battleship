c7cd5f14591af119150019912cbfe1950275af69
Mariusz <casmar90@gmail.com>
2023-12-15 16:35:55 +0100

    Refactor

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -9,32 +9,32 @@ describe('gameBoardFactory', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '1', 'horizontal');
 
-		expect(gameBoard.getCell('A', '1').status && gameBoard.getCell('B', '1').status && gameBoard.getCell('C', '1').status).toBe('taken');
+		expect(gameBoard.getCell('A', '1') && gameBoard.getCell('B', '1') && gameBoard.getCell('C', '1')).toBe('taken');
 	});
 
 	it('should place ship at specific coordinates, vertical', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '1', 'vertical');
 
-		expect(gameBoard.getCell('A', '1').status && gameBoard.getCell('A', '2').status && gameBoard.getCell('A', '3').status).toBe('taken');
+		expect(gameBoard.getCell('A', '1') && gameBoard.getCell('A', '2') && gameBoard.getCell('A', '3')).toBe('taken');
 	});
 
 	it('should give error when placing ship outside board, horizontal', () => {
 		const gameBoard = gameBoardFactory();
 
-		expect(gameBoard.placeShip(ship, 'J', '5', 'horizontal')).toBe("Can't place ship");
+		expect(gameBoard.placeShip(ship, 'J', '5', 'horizontal')).toBe("Can't place ship out of board");
 	});
 
 	it('should give error when placing ship outside board, vertical', () => {
 		const gameBoard = gameBoardFactory();
 
-		expect(gameBoard.placeShip(ship, 'B', '10', 'vertical')).toBe("Can't place ship");
+		expect(gameBoard.placeShip(ship, 'B', '10', 'vertical')).toBe("Can't place ship out of board");
 	});
 
 	it('should give error when another ship occupy space', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '8', 'horizontal');
 
-		expect(gameBoard.placeShip(ship, 'A', '8', 'horizontal')).toBe("Can't place ship");
+		expect(gameBoard.placeShip(ship, 'A', '8', 'horizontal')).toBe("Can't place ship, cells are not empty");
 	});
 });
