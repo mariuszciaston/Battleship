465e953615ed6734cd2e6a534c4b34fcbd810c3e
Mariusz <casmar90@gmail.com>
2023-12-17 04:16:47 +0100

    Add receiveAttack

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,3 +1,5 @@
+import shipFactory from './ship';
+
 const gameBoardFactory = () => {
 	const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 	const rows = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
@@ -8,7 +10,7 @@ const gameBoardFactory = () => {
 			array[i] = [];
 
 			for (let j = 0; j < 10; j += 1) {
-				array[i][j] = { col: cols[j], row: rows[i], status: 'empty' };
+				array[i][j] = { col: cols[j], row: rows[i], status: 'empty', takenBy: 'none' };
 			}
 		}
 	};
@@ -21,10 +23,10 @@ const gameBoardFactory = () => {
 			return 'Out of board';
 		}
 
-		return array[rowIndex][colIndex].status;
+		return array[rowIndex][colIndex];
 	};
 
-	const setCell = (col, row, newStatus) => {
+	const setCell = (col, row, newStatus, newTakenBy) => {
 		const colIndex = cols.indexOf(col);
 		const rowIndex = rows.indexOf(row);
 
@@ -34,7 +36,11 @@ const gameBoardFactory = () => {
 
 		array[rowIndex][colIndex].status = newStatus;
 
-		return array[rowIndex][colIndex].status;
+		if (newTakenBy) {
+			array[rowIndex][colIndex].takenBy = newTakenBy;
+		}
+
+		return array[rowIndex][colIndex];
 	};
 
 	const placeShip = (ship, col, row, orientation) => {
@@ -42,28 +48,43 @@ const gameBoardFactory = () => {
 		const cells = isHorizontal ? cols : rows;
 		const start = cells.indexOf(isHorizontal ? col : row);
 
-		if (start < 0 || start + ship.shipLength > cells.length) {
+		if (start < 0 || start + ship.size > cells.length) {
 			return "Can't place ship out of board";
 		}
 
-		for (let i = 0; i < ship.shipLength; i += 1) {
+		for (let i = 0; i < ship.size; i += 1) {
 			const currentCol = isHorizontal ? cells[start + i] : col;
 			const currentRow = isHorizontal ? row : cells[start + i];
-			const cellStatus = getCell(currentCol, currentRow);
+			const cellStatus = getCell(currentCol, currentRow).status;
 
 			if (cellStatus !== 'empty') {
 				return "Can't place ship, cells are not empty";
 			}
 
-			setCell(currentCol, currentRow, 'taken');
+			setCell(currentCol, currentRow, 'taken', ship);
 		}
 
 		return 'Ship placed successfully';
 	};
 
+	const receiveAttack = (col, row) => {
+		const cell = getCell(col, row);
+
+		if (cell.status === 'taken') {
+			cell.takenBy.hit();
+			setCell(col, row, 'hit');
+			return cell.status;
+		}
+		if (cell.status === 'empty') {
+			setCell(col, row, 'miss');
+			return cell.status;
+		}
+		return null;
+	};
+
 	generateArray();
 
-	return { getCell, placeShip };
+	return { getCell, placeShip, receiveAttack };
 };
 
 export default gameBoardFactory;
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-undef */
-
+import shipFactory from '../modules/ship';
 import gameBoardFactory from '../modules/gameBoard';
 
-describe('gameBoardFactory', () => {
-	const ship = { shipLength: 3 };
+describe('Ship placing', () => {
+	const ship = shipFactory('Destroyer');
 
 	it('should place ship at specific coordinates, horizontal', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '1', 'horizontal');
 
-		expect(gameBoard.getCell('A', '1') && gameBoard.getCell('B', '1') && gameBoard.getCell('C', '1')).toBe('taken');
+		expect(gameBoard.getCell('A', '1').status && gameBoard.getCell('B', '1').status && gameBoard.getCell('C', '1').status).toBe('taken');
 	});
 
 	it('should place ship at specific coordinates, vertical', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '1', 'vertical');
 
-		expect(gameBoard.getCell('A', '1') && gameBoard.getCell('A', '2') && gameBoard.getCell('A', '3')).toBe('taken');
+		expect(gameBoard.getCell('A', '1').status && gameBoard.getCell('A', '2').status && gameBoard.getCell('A', '3').status).toBe('taken');
 	});
 
 	it('should give error when placing ship outside board, horizontal', () => {
@@ -31,10 +31,32 @@ describe('gameBoardFactory', () => {
 		expect(gameBoard.placeShip(ship, 'B', '10', 'vertical')).toBe("Can't place ship out of board");
 	});
 
-	it('should give error when another ship occupy space', () => {
+	it('should give error when another ship occupy space or cells are not empty', () => {
 		const gameBoard = gameBoardFactory();
 		gameBoard.placeShip(ship, 'A', '8', 'horizontal');
 
 		expect(gameBoard.placeShip(ship, 'A', '8', 'horizontal')).toBe("Can't place ship, cells are not empty");
+	});
+});
+
+describe('Receiving attack', () => {
+	const ship = shipFactory('Destroyer');
+
+	it('should receive attack on ships', () => {
+		const gameBoard = gameBoardFactory();
+		gameBoard.placeShip(ship, 'A', '1', 'horizontal');
+
+		expect(gameBoard.receiveAttack('A', '1')).toBe('hit');
+		expect(gameBoard.receiveAttack('B', '1')).toBe('hit');
+		expect(gameBoard.receiveAttack('C', '1')).toBe('hit');
+	});
+
+	it('should result in miss when shot is missed', () => {
+		const gameBoard = gameBoardFactory();
+		gameBoard.placeShip(ship, 'A', '1', 'horizontal');
+
+		expect(gameBoard.receiveAttack('D', '2')).toBe('miss');
+		expect(gameBoard.receiveAttack('E', '3')).toBe('miss');
+		expect(gameBoard.receiveAttack('F', '4')).toBe('miss');
 	});
 });
