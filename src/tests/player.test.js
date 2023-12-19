b477d4ecaae80465952acd9d7ec075d668e4ba12
Mariusz <casmar90@gmail.com>
2023-12-19 19:26:41 +0100

    Change naming and split test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,30 +1,30 @@
 /* eslint-disable no-undef */
 
 import shipFactory from '../modules/ship';
-import { humanGameboard, computerGameboard } from '../modules/gameBoard';
+import gameboardFactory from '../modules/gameboard';
 import playerFactory from '../modules/player';
 
 describe('playerFactory', () => {
-	let humanPlayer;
-	let computerPlayer;
-
 	const ship = shipFactory('Submarine');
 
-	beforeEach(() => {
-		humanPlayer = playerFactory();
-		computerPlayer = playerFactory();
-	});
+	const computerGameboard = gameboardFactory();
+	const humanGameboard = gameboardFactory();
 
-	test('humanPlayer attack', () => {
+	const humanPlayer = playerFactory();
+	const computerPlayer = playerFactory();
+
+	test('humanPlayer attack and miss', () => {
 		expect(humanPlayer.attack('A', '1', computerGameboard)).toBe('miss');
 		expect(humanPlayer.attack('A', '1', computerGameboard)).toBe('already shot');
+	});
 
+	test('humanPlayer attack and hit', () => {
 		computerGameboard.placeShip(ship, 'A', '3', 'horizontal');
 		expect(humanPlayer.attack('A', '3', computerGameboard)).toBe('hit');
 		expect(humanPlayer.attack('A', '3', computerGameboard)).toBe('already shot');
 	});
 
-	test('computerPlayer randomAttack', () => {
+	test('computerPlayer randomAttack on empty board', () => {
 		expect(computerPlayer.randomAttack(humanGameboard)).toBe('miss');
 	});
 });
