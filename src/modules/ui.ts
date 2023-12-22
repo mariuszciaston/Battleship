738acbf92f4b69a0633670fb144d4e38dba275c4
Mariusz <casmar90@gmail.com>
2023-12-22 14:12:04 +0100

    Add renderBoard()

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -1,7 +1,39 @@
-import populateGameboard from './game';
+import { Gameboard } from './types';
+
+import { humanGameboard, computerGameboard } from './game';
 
 const ui = () => {
-	populateGameboard();
+	const wrapper = document.createElement('div');
+	wrapper.id = 'wrapper';
+	document.body.append(wrapper);
+
+	const renderBoard = (gameboard: Gameboard) => {
+		const board = document.createElement('div');
+		board.classList.add('board');
+
+		if (gameboard === humanGameboard) {
+			board.id = 'firstBoard';
+		}
+
+		if (gameboard === computerGameboard) {
+			board.id = 'secondBoard';
+		}
+
+		gameboard.array.forEach((row) => {
+			row.forEach((col) => {
+				const cell = document.createElement('div');
+				cell.classList.add('cell');
+				cell.classList.add(col.status);
+				cell.setAttribute('data-col', col.col);
+				cell.setAttribute('data-row', col.row);
+				board.append(cell);
+			});
+		});
+		wrapper.append(board);
+	};
+
+	const firstBoard = renderBoard(humanGameboard);
+	const secondBoard = renderBoard(computerGameboard);
 };
 
 export default ui;
