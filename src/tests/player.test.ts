f651819e2f235abcf99fb29a8df75f3995cbea46
Mariusz <casmar90@gmail.com>
2024-01-10 13:44:02 +0100

    Add computer followupAttack test

--- src/tests/player.test.ts
+++ src/tests/player.test.ts
@@ -3,7 +3,8 @@ import shipFactory from '../modules/ship';
 import playerFactory from '../modules/player';
 
 describe('playerFactory', () => {
-	const ship = shipFactory('Submarine');
+	const submarine = shipFactory('Submarine');
+	const carrier = shipFactory('Carrier');
 
 	const computerGameboard = gameboardFactory();
 	const humanGameboard = gameboardFactory();
@@ -17,12 +18,26 @@ describe('playerFactory', () => {
 	});
 
 	test('human attack and hit', () => {
-		computerGameboard.placeShip(ship, 'A', '3', 'horizontal');
+		computerGameboard.placeShip(submarine, 'A', '3', 'horizontal');
 		expect(human.attack(computerGameboard, 'A', '3')).toBe('hit');
 		expect(human.attack(computerGameboard, 'A', '3')).toBe('already shot');
 	});
 
 	test('computer randomAttack on empty board', () => {
 		expect(computer.randomAttack(humanGameboard).result).toBe('miss');
+	});
+
+	test('computer followupAttack', () => {
+		humanGameboard.placeShip(carrier, 'A', '1', 'horizontal');
+		computer.attack(humanGameboard, 'A', '1');
+		computer.followupAttack(humanGameboard, 'A', '1');
+
+		if (humanGameboard.getCell('A', '2').status === 'miss') {
+			computer.followupAttack(humanGameboard, 'A', '1');
+
+			expect(humanGameboard.getCell('B', '1').status).toBe('hit');
+		} else {
+			expect(humanGameboard.getCell('B', '1').status).toBe('hit');
+		}
 	});
 });
