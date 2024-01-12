2c639fb4758406279bf14589ee66da2849fa26bd
Mariusz <casmar90@gmail.com>
2024-01-12 10:55:35 +0100

    Add tests for vertical ships & refactor

--- src/tests/player.test.ts
+++ src/tests/player.test.ts
@@ -31,6 +31,7 @@ describe('playerFactory', () => {
 
 	test('human attack and hit', () => {
 		computerGameboard.placeShip(submarine, 'A', '3', 'horizontal');
+
 		expect(human.attack(computerGameboard, 'A', '3')).toBe('hit');
 		expect(human.attack(computerGameboard, 'A', '3')).toBe('already shot');
 	});
@@ -41,12 +42,12 @@ describe('playerFactory', () => {
 
 	test('computer followupAttack', () => {
 		humanGameboard.placeShip(carrier, 'A', '1', 'horizontal');
+
 		computer.attack(humanGameboard, 'A', '1');
 		computer.followupAttack(humanGameboard, 'A', '1');
 
 		if (humanGameboard.getCell('A', '2').status === 'miss') {
 			computer.followupAttack(humanGameboard, 'A', '1');
-
 			expect(humanGameboard.getCell('B', '1').status).toBe('hit');
 		} else {
 			expect(humanGameboard.getCell('B', '1').status).toBe('hit');
@@ -59,19 +60,13 @@ describe('playerFactory', () => {
 		computer.attack(humanGameboard, 'A', '1');
 		computer.attack(humanGameboard, 'B', '1');
 
-		computer.prevHit = { col: 'A', row: '1' };
-		computer.lastHit = { col: 'B', row: '1' };
-
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('C', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('D', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('E', '1').status).toBe('hit');
 	});
 
@@ -81,23 +76,16 @@ describe('playerFactory', () => {
 		computer.attack(humanGameboard, 'D', '1');
 		computer.attack(humanGameboard, 'E', '1');
 
-		computer.prevHit = { col: 'D', row: '1' };
-		computer.lastHit = { col: 'E', row: '1' };
-
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('F', '1').status).toBe('miss');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('C', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('B', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
 	});
 
@@ -107,23 +95,16 @@ describe('playerFactory', () => {
 		computer.attack(humanGameboard, 'C', '1');
 		computer.attack(humanGameboard, 'D', '1');
 
-		computer.prevHit = { col: 'C', row: '1' };
-		computer.lastHit = { col: 'D', row: '1' };
-
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('E', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('F', '1').status).toBe('miss');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('B', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
 	});
 
@@ -133,23 +114,89 @@ describe('playerFactory', () => {
 		computer.attack(humanGameboard, 'D', '1');
 		computer.attack(humanGameboard, 'C', '1');
 
-		computer.prevHit = { col: 'D', row: '1' };
-		computer.lastHit = { col: 'C', row: '1' };
-
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('E', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('F', '1').status).toBe('miss');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
-
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('B', '1').status).toBe('hit');
 
-		computer.finishingAttack(humanGameboard, computer.lastHit.col, computer.lastHit.row, computer.prevHit);
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
+	});
 
+	test('computer finishingAttack from up to down', () => {
+		humanGameboard.placeShip(carrier, 'A', '1', 'vertical');
+
+		computer.attack(humanGameboard, 'A', '1');
+		computer.attack(humanGameboard, 'A', '2');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '3').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '4').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '5').status).toBe('hit');
+	});
+
+	test('computer finishingAttack from down to down', () => {
+		humanGameboard.placeShip(carrier, 'A', '1', 'vertical');
+
+		computer.attack(humanGameboard, 'A', '4');
+		computer.attack(humanGameboard, 'A', '5');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '6').status).toBe('miss');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '3').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '2').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
+	});
+
+	test('computer finishingAttack from middle to down 1', () => {
+		humanGameboard.placeShip(carrier, 'A', '1', 'vertical');
+
+		computer.attack(humanGameboard, 'A', '3');
+		computer.attack(humanGameboard, 'A', '4');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '5').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '6').status).toBe('miss');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '2').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
+	});
+
+	test('computer finishingAttack from middle to down 2', () => {
+		humanGameboard.placeShip(carrier, 'A', '1', 'vertical');
+
+		computer.attack(humanGameboard, 'A', '4');
+		computer.attack(humanGameboard, 'A', '3');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '5').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '6').status).toBe('miss');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
+		expect(humanGameboard.getCell('A', '2').status).toBe('hit');
+
+		computer.finishingAttack(humanGameboard, computer.getLastHit().col, computer.getLastHit().row, computer.getPrevHit());
 		expect(humanGameboard.getCell('A', '1').status).toBe('hit');
 	});
 });
